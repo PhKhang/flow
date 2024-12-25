b0ea716bd802741efad817578e5c6db3ea956de7
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2024-12-25 17:42:32 +0700

    Fix bug in follow pagination

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -2,7 +2,7 @@ import express from "express"
 const apiRouter = express.Router();
 // import { init, showList, showDetails } from '../controllers/blogController.js';
 import {addComment, likeComment, unlikeComment, getCommentsByPost} from '../controller/commentController.js';
-import {addPost, likePost, searchPosts, unlikePost, getPostsByAuthor, getAllPostsPagination} from '../controller/postController.js';
+import {addPost, likePost, searchPosts, unlikePost, getPostsByAuthor, getAllPostsPagination, getFollowPostsPagination} from '../controller/postController.js';
 import {searchUsersByName} from '../controller/userController.js';
 import {followUser, unfollowUser} from '../controller/followController.js';
 import {createNotification, deleteNotification, updateReadStatus, updateUnreadStatus} from '../controller/notificationController.js';
@@ -175,6 +175,25 @@ apiRouter.get("/posts", async (req, res) => {
     try {
         const { offset = 0, limit = 10, userId } = req.query;
         const posts = await getAllPostsPagination(userId, parseInt(limit), parseInt(offset));
+        const plainPosts = posts.map(post => post.toObject());
+        
+        // Add the timeAgo property to each plain object
+        plainPosts.forEach(post => {
+            const { formattedDate, timeAgo } = formatPostDate(post.created_at);
+            post.timeAgo = timeAgo;
+            post.modified_created_at = formattedDate;
+        });
+
+        res.json(plainPosts);
+    } catch (error) {
+        res.status(500).json({ error: 'Error loading posts', posts: posts });
+    }
+});
+
+apiRouter.get("/followPosts", async (req, res) => {
+    try {
+        const { offset = 0, limit = 10, userId } = req.query;
+        const posts = await getFollowPostsPagination(userId, parseInt(limit), parseInt(offset));
         const plainPosts = posts.map(post => post.toObject());
         
         // Add the timeAgo property to each plain object
